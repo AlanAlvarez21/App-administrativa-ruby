c31c399157d12eccf7ab054e30eaa1f51e669b55
AlanAlvarez21 <alandanielalvarez0000@gmail.com>
2020-08-12 17:50:04 -0500

    Google cloud storage added

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+    config.fog_credentials = {
+        provider:                         'Google',
+        google_storage_access_key_id:     'GOOGH4ZLOQEZONXXIYKJBIZB',
+        google_storage_secret_access_key: 'xjBZxadzTREmVzgokMxruP56C3of3MbsXlI2xIJP'    
+    }
+    config.fog_directory = 'scenic-scholar-286221'
+end
