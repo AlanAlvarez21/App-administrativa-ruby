4850af2f5e81ab585476ad1be3abb8c718eb0a99
AlanAlvarez21 <alandanielalvarez0000@gmail.com>
2020-08-16 20:18:10 -0500

    commit antes de deploy

--- Gemfile
+++ Gemfile
@@ -74,7 +74,6 @@ group :test do
 end
 
 group :production do 
-
  gem 'pg'
  gem 'rails_12factor'
 end 
--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module AppComercial
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.Initialize_on_precompile = false 
+    config.serve_static_assets = true
   end
 end
