3c247f3d7ec2f9d4efda35b5a6fedb7f47021a94
AlanAlvarez21 <alandanielalvarez0000@gmail.com>
2020-08-20 17:32:46 -0500

    make it better

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController 
 
     def results 
-            @productos = Product.buscador(params[:termino])
-            @clientes = Client.buscador(params[:termino])
-            @proveedores = Supplier.buscador(params[:termino])
+        @productos = Product.buscador(params[:termino])
+        @clientes = Client.buscador(params[:termino])
+        @proveedores = Supplier.buscador(params[:termino])
     end 
 
 end 
